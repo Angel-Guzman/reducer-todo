495544ae6461b7e73253b7f21c7df6ce686e7810
Angelg10 <angelguzman97@gmail.com>
2020-05-11 15:42:25 -0400

    reducer, and initial state created/imported

---
+++ todo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { reducer, tasks } from './reducers/reducer.js';
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
+
+export const tasks = [
+  {
+    task: 'Clean Room',
+    id: '23458976',
+    completed: false
+  },
+  {
+    task: 'Workout',
+    id: '98769234',
+    completed: false
+  },
+  {
+    task: 'Finish Assignment',
+    id: '57896263',
+    completed: false
+  }
+];

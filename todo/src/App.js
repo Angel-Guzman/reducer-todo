4ed4393a351fbd268407ef7aa56a09966b9d3ee3
Angelg10 <angelguzman97@gmail.com>
2020-05-11 15:50:06 -0400

    state set up, todos rendered

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { reducer, tasks } from './reducers/reducer.js';
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, tasks)
   
   return (
     <div>
       <h1>Todo App</h1>
-      <div>
-
+      <div className="tasks">
+        {tasks.map(item => {
+          return <p>{item.task}</p>
+        })}
+      </div>
+      <div className="todo-form">
+        
       </div>
     </div>
   );
